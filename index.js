adf539dbffda65a693cb2d667d06c95b05d90789
Dev-MarcoLira <malirab2302@gmail.com>
2024-08-08 13:30:21 -0300

    add admin routes

--- index.js
+++ index.js
@@ -12,12 +12,19 @@ const PASSWORD = process.env.PASSWORD
 const uri = `mongodb+srv://${USER}:${PASSWORD}@cluster1.kowtsmr.mongodb.net/dankicode?retryWrites=true&w=majority&appName=Cluster1`
 const Posts = require('./schemas/Posts')
 
+const session = require('express-session')
+
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     console.log('Connected')
 }).catch((err)=>{
     console.log(err.message)
 })
+
+app.use(session({
+    secret: 'keyboard cat',
+    cookie: { maxAge: 6000000 }
+}))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -27,7 +34,102 @@ app.set('view engine', 'html')
 app.use('/public', express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, '/pages'))
 
-app.get('/', async(req, res) => {
+const users = [
+    {
+        login: 'Marco',
+        password: '123456'
+    }
+] 
+
+app.get('/admin/news', (req,res)=>{
+
+    Posts.find({}).sort({ '_id': -1 }).exec()
+        .then(posts => {
+    
+            posts = posts.map(post => {
+
+                return{
+                    id: post._id,
+                    title: post.title,
+                    category: post.category,
+                    body: post.body.substring(0, 100) + '...',
+                    views: post.views,
+                    image: post.image,
+        //            slug: post.slug
+                }
+
+            })
+            
+            res.render('admin-news', { posts })
+            
+        })
+
+})
+
+app.post('/admin/news', (req,res)=>{
+
+    //console.log(req.body)
+
+    Posts.create({
+        title: req.body.title,
+        image: req.body.url_image,
+        category: req.body.category,
+        body: req.body.body,
+        slug: req.body.slug,
+        author: req.body.author,
+        views: 0
+
+    })
+
+
+    res.redirect('/admin/news')
+    
+})
+
+app.get('/admin/news/delete/:id', (req,res)=>{
+
+    Posts.deleteOne({ _id: req.params.id })
+        .then(()=>{
+
+            res.redirect('/admin/news')
+        })
+        .catch(err=>{
+            console.log(err.message)
+        })
+
+
+})
+
+app.post('/admin/login', (req, res) => {
+
+    const login = req.body.login
+    const password = req.body.password
+
+    users.forEach(user=>{
+        if(user.login == login && user.password == password){
+            req.session.login = login
+        }
+    })
+
+    
+    res.redirect('/admin/login')
+})
+
+app.get('/admin/login', (req, res)=>{
+
+    if(!req.session.login){
+        
+        res.render('admin-login')
+        
+    }else{
+        res.render('admin-panel')
+    }
+
+
+})
+
+
+app.get('/', (req, res) => {
 
     const search = req.query.search
 
